b23e540da1adc72f6dd90691148e7938a219314b
Jonas Wolfram <jonas.wolfram@me.com>
2022-05-31 13:28:44 +0200

    New Folder Structure - Basic Layout Side/NavBar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
@@ -22,7 +22,52 @@ const App = () => {
               </button>
             </TooltipComponent>
           </div>
-          {activeMenu ? <div>Sidebar</div> : <div>sidebar w-3</div>}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              Sidebar {/* <Sidebar /> */}
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              Sidebar {/* <Sidebar /> */}
+            </div>
+          )}
+          <div
+            className={
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              NavBar {/* <Navbar /> */}
+            </div>
+
+            <div>
+              <Routes>
+                {/* Dashbord */}
+                <Route path="/" element="Index" />
+                <Route path="/ecommerce" element="ECommerce" />
+                {/* Pages */}
+                <Route path="/bestellungen" element="Bestellungen" />
+                <Route path="/angestellte" element="Angestellten" />
+                <Route path="/kunden" element="Kunden" />
+                {/* Apps */}
+                {/* <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} /> */}
+
+                {/* charts  */}
+                {/* <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} /> */}
+              </Routes>
+            </div>
+          </div>
         </div>
       </BrowserRouter>
     </div>
