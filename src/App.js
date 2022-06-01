8b2c9e9b7e65a21d865ed5142facd03e2c65b97a
Jonas Wolfram <jonas.wolfram@me.com>
2022-06-01 14:00:00 +0200

    Sidebar Content + File Structure (Components/Pages) added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { NavBar, Sidebar } from "./components";
+import {
+  Ecommerce,
+  Bestellungen,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from "./pages";
 import "./App.css";
 
 const App = () => {
@@ -24,11 +43,11 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-              Sidebar {/* <Sidebar /> */}
+              <Sidebar />
             </div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg">
-              Sidebar {/* <Sidebar /> */}
+              <Sidebar />
             </div>
           )}
           <div
@@ -38,33 +57,35 @@ const App = () => {
                 : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
             }>
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-              NavBar {/* <Navbar /> */}
+              <NavBar />
             </div>
 
             <div>
               <Routes>
-                {/* Dashbord */}
-                <Route path="/" element="Index" />
-                <Route path="/ecommerce" element="ECommerce" />
-                {/* Pages */}
-                <Route path="/bestellungen" element="Bestellungen" />
-                <Route path="/angestellte" element="Angestellten" />
-                <Route path="/kunden" element="Kunden" />
-                {/* Apps */}
-                {/* <Route path="/kanban" element={<Kanban />} />
-              <Route path="/editor" element={<Editor />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/color-picker" element={<ColorPicker />} /> */}
+                {/* dashboard  */}
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
+
+                {/* pages  */}
+                <Route path="/bestellungen" element={<Bestellungen />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
+
+                {/* apps  */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
 
                 {/* charts  */}
-                {/* <Route path="/line" element={<Line />} />
-              <Route path="/area" element={<Area />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/financial" element={<Financial />} />
-              <Route path="/color-mapping" element={<ColorMapping />} />
-              <Route path="/pyramid" element={<Pyramid />} />
-              <Route path="/stacked" element={<Stacked />} /> */}
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
               </Routes>
             </div>
           </div>
---
+++ src/pages/Bestellungen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bestellungen = () => {
+  return <div>Bestellungen</div>;
+};
+
+export default Bestellungen;
