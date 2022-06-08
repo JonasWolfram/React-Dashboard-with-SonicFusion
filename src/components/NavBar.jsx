52630305d1dda0329a5836b6e0a73fef1423b8c7
Jonas Wolfram <jonas.wolfram@me.com>
2022-06-08 13:27:23 +0200

    NavBar Imports

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { ToolipComponent } from "@syncfusion/ej2-react-popups";
+
+import avatar from "../data/avatar.jpg";
+import { Cart, Chat, Notification, UserProfile } from ".";
+import { useStateContext } from "../context/ContextProvider";
 
 const NavBar = () => {
   return <div>NavBar</div>;
